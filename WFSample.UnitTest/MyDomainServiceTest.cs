8ec520c6c2bc587f8ced010174c7ee9b8d8f7643
Andrey Nikiforov <andreyn@magenic.com>
2013-12-26 17:23:36 -0800

    adding unit tests for business logic

---
+++ WFSample.UnitTest/MyDomainServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WFSample.Entities;
+using WFSample.Services;
+
+namespace WFSample.UnitTest
+{
+    [TestClass]
+    public class MyDomainServiceTest
+    {
+        [TestMethod]
+        public void CountCustomer_SHOULD_CallRepoForApprovedCustomers()
+        {
+            var repo = new Mock<IMyCustomerRepository>(MockBehavior.Strict);
+
+            //setup mock -- only those behavior we are planning to use in THIS particular unit test
+            var expectedCounter = 12345;
+            repo.Setup(m => m.GetCustomerCountByStatus(CustomerApprovalStatus.Approved)).Returns<int>(_ => expectedCounter);
+
+            var service = new MyDomainService(repo.Object);
+
+            //run test
+            var result = service.CountCustomers();
+
+            Assert.AreEqual(expectedCounter, result);   //confirming that service passing number from repo call without changes
+
+            //validating that all calls for repo were actually called
+            repo.VerifyAll();
+        }
+    }
+}
